bfa19d9891d13fe5cb86d19a586d59f79933250a
WE-PC <cab-1986@yandex.ru>
2023-02-25 15:47:35 +0300

    Add project files.

---
+++ DZ6-1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DZ6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var venus = new { Name = "Венера", Number = 2, EquatorLenth = 38052, PreviousPlanet = "null" };
+            var earth = new { Name = "Земля", Number = 3, EquatorLenth = 40076, PreviousPlanet = venus };
+            var mars = new { Name = "Марс", Number = 4, EquatorLenth = 21344, PreviousPlanet = earth };
+
+            Print(venus);
+            Print(earth);
+            Print(mars);
+            Print(venus);
+
+            bool EqualToVenus(object planet)
+            {
+                return planet.Equals(venus);
+            }
+
+            void Print(object planet)
+            {
+                Console.WriteLine(planet + " Equal : " + EqualToVenus(planet));
+            }
+        }
+    }
+}
